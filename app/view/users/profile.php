e9efa70af3dbf981c9bab6cda01f12f240e2251e
Koen <cheesewheel1573@gmail.com>
2020-02-13 20:22:59 +0100

    usertype on profile

--- app/view/users/profile.php
+++ app/view/users/profile.php
@@ -9,6 +9,26 @@
             <p><?php echo $_SESSION['studentNumber'] ?></p>
             <p class="profileHeader">Email</p>
             <p><?php echo $_SESSION['email'] ?></p>
+            <p class="profileHeader">User Type</p>
+            <?php
+                switch($_SESSION['userType']){
+                    case 1:
+                        echo "Regular user";
+                    break;
+    
+                    case 2:
+                        echo "Admin";
+                    break;
+                    
+                    case 3:
+                        echo "Superadmin";
+                    break;
+                    
+                    default:
+                        echo "Regular user";
+                    break;
+                }
+            ?>
         <div>
 
         <!-- Threads posted by the user -->
